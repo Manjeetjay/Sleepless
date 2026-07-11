09cf4949f2669ea55e4a046b098f256ee83611e6
Manjeetjay <manjeetjay00@gmail.com>
2026-07-11 18:04:09 +0530

    Resolved the incorrect import issue

--- backend/src/main/java/com/devs/sleepless/controller/RedirectController.java
+++ backend/src/main/java/com/devs/sleepless/controller/RedirectController.java
@@ -2,6 +2,7 @@ package com.devs.sleepless.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.beans.factory.annotation.Value;
 
 @Controller
 public class RedirectController {
